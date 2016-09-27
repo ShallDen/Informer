59234bffcb2d4674ee84f350dfc24cd537fd649e
Den Mayasov <shallden@yandex.ru>
2016-09-27 12:32:30 +0300

    Fix of issue with EF and app.config files

--- Informer.DataAccess/InformerContext.cs
+++ Informer.DataAccess/InformerContext.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.Entity;
 using OpenWeatherMap;
+using SqlProviderServices = System.Data.Entity.SqlServer.SqlProviderServices;
 
 namespace Informer.DataAccess
 {
